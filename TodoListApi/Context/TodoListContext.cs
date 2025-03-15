50f8cb00b341340383a362bdcb10987594fa5d78
Ramshley P. <ramshleyv@gmail.com>
2025-03-15 13:08:04 -0400

    Adding Context folder and class dbContext

---
+++ TodoListApi/Context/TodoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Context
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> dbContext) : base(dbContext) {}
+
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
