a88c786b0630035ae1eff3d7f0e85f6a9ecf8772
Ramshley P. <ramshleyv@gmail.com>
2025-03-17 14:34:26 -0400

    Modificación en la interfaz ITask: se agregó un CRUD a la interfaz para ser implementada.

--- TodoListApi/Interfeces/ITask.cs
+++ TodoListApi/Interfeces/ITask.cs
@@ -5,5 +5,11 @@ namespace TodoListApi.Interfeces
     public interface ITask
     {
         List<TodoTasks> GetTasks();
+        List<TodoTasks> GetTaskByIdUser(int id);
+        TodoTasks GetTaskById(int id);
+        TodoTasks CreateTask(TodoTasks task);
+        TodoTasks UpdateTask(TodoTasks task);
+        bool DeleteTask(int id);
+
     }
 }
