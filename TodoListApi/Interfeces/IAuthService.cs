7e51d5c9cce684458ee86f6745d45597f195abfc
Ramshley P. <ramshleyv@gmail.com>
2025-03-17 18:31:32 -0400

    Agregación: se agregó una interfaz de auth.

---
+++ TodoListApi/Interfeces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using TodoListApi.DTOs;
+using TodoListApi.Models;
+
+namespace TodoListApi.Interfeces
+{
+    public interface IAuthService
+    {
+        Task<UserRegisterDto> UserRegister(UserRegisterDto userDto);
+        Task<UserLoginDto> UserLogin(UserLoginDto userDto);
+        bool VerifyPasswordHash(string password, string storedHash);
+    }
+}
