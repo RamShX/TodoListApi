6d27a099d5a5eb5393fa0d2a3342ce7cedf983f4
Ramshley P. <ramshleyv@gmail.com>
2025-03-17 18:25:01 -0400

    Agregación: Se agregó DTOs de user

---
+++ TodoListApi/DTOs/UserRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListApi.DTOs
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
