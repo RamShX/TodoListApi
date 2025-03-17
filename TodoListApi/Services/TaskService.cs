f0595089cb3ef2029f7816af001b01b3527e74b6
Ramshley P. <ramshleyv@gmail.com>
2025-03-17 01:21:57 -0400

    Carpeta de servicios y las clases servicios task y user

---
+++ TodoListApi/Services/TaskService.cs
@@ -0,0 +1,12 @@
+﻿using TodoListApi.Interfeces;
+
+namespace TodoListApi.Services
+{
+    public class TaskService : ITask
+    {
+        public List<Task> GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoListApi/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using TodoListApi.Interfeces;
+using TodoListApi.Models;
+
+namespace TodoListApi.Services
+{
+    public class UserService : IUser
+    {
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
